144f014b568e0e64e02941394c2d568b691822c8
root <root@SURFACE.>
2024-05-01 16:15:43 +0300

    code-along

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
-
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    }, 
+    {
+      path: "/about",
+      element: <About />
+    },
+    {
+      path: "/login",
+      element: <Login />
+    },
+    {
+      path: "/profile/:id",
+      element: <UserProfile />
+    }
+  ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Home />);
